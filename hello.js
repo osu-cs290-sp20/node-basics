cdc4f4535a3514c3a91d8b52d85376f4d90e5a05
Rob Hess <robwhess@gmail.com>
2020-05-11 09:57:44 -0700

    Add code from 5/11 lecture.

---
+++ hello.js
@@ -0,0 +1,16 @@
+console.log("Hello, world!");
+
+/* process */
+console.log("== process.env.SOME_ENV_VARIABLE:", process.env.SOME_ENV_VARIABLE);
+
+// var circumference = require('./circle');
+// console.log("== circumference:", circumference);
+// console.log("== circumference(5):", circumference(5));
+
+var circle = require('./lib/circle');
+console.log("== circle:", circle);
+console.log("== circle.circumference(5):", circle.circumference(5));
+console.log("== circle.area(5):", circle.area(5));
+
+
+var figlet = require('figlet');
